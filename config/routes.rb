506447962af54983a41fe5b613d2e2374c33888a
jfagan132 <jfagan@ggc.edu>
2016-02-25 17:53:56 +0000

    added signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'main#home'
   get 'signup' => 'users#new'
+  resources :users
   
 
   # The priority is based upon order of creation: first created -> highest priority.
