d1213748f0feff2c82c90441ca557c367d688e7e
jfagan132 <jfagan@ggc.edu>
2016-03-18 23:07:09 +0000

    added admin users and user deletion

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create!(name:  "Jacob Fagan",
+             email: "jfagan@ggc.edu",
+             password:              "iseekthetruth12",
+             password_confirmation: "iseekthetruth12",
+             admin: true)
+             
+99.times do |n|
+  name  = "Example #{n+1}"
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
